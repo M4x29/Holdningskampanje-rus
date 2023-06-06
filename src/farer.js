9fbccd5271255fcc816f1f12d8ad5638d2c1b173
mpu69 <elura001@osloskolen.no>
2023-06-06 11:28:54 +0200

    xdd

--- src/farer.js
+++ src/farer.js
@@ -1,3 +1,15 @@
 const helse = document.getElementById("helse");
 const avhengighet = document.getElementById("avhengighet");
 const ulykker = document.getElementById("ulykker");
+
+const helseText = document.getElementById("helseText");
+const avhengighetText = document.getElementById("avhengighetText");
+const ulykkerText = document.getElementById("ulykkerText");
+
+helse.addEventListener("mouseenter", () => {
+  helseText.style.display = "block";
+});
+
+helse.addEventListener("mouseleave", () => {
+  helseText.style.display = "none";
+});
